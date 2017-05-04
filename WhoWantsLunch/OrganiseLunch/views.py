abae13740abdfe0e2b55b348b963c91fdc1eb73b
Ryan Warren <ryanz213@gmail.com>
2017-05-04 11:10:23 +0100

    fix views

--- WhoWantsLunch/OrganiseLunch/views.py
+++ WhoWantsLunch/OrganiseLunch/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def Lunches(request):
-   return render(request, "./meal_details.html", {})
+   return render(request, "./OrganiseLunch/meal_details.html", {})
