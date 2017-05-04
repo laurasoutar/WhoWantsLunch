ace25c1e987242e3ed14c332c998b72062370660
Scott Culcheth <scott.culcheth@gmail.com>
2017-05-04 11:39:55 +0100

    Refactor teamkey into config

--- WhoWantsLunch/SlackBot/management/commands/listener.py
+++ WhoWantsLunch/SlackBot/management/commands/listener.py
@@ -1,23 +1,26 @@
 ### slackbot/management/commands/listener.py
 from slackclient import SlackClient
 from django.core.management.base import BaseCommand
+from SlackBot.apps import SlackbotConfig
 import time
 class Command(BaseCommand):
     help = 'Starts the bot for the first'
     def handle(self, *args, **options):
-        client = SlackClient('xoxb-179408173191-E8sjwhEkQf43ZUUdmMTyeUYD')
+        client = SlackClient(SlackbotConfig.team_key)
         if client.rtm_connect():
             while True:
                 events = client.rtm_read()
                 for event in events:
                     try:
-                        if event['type']=='message' and event['text']=='hi':
-                            client.rtm_send_message(
-                                event['channel'],
-                                "Hello World!"
-                            )
+                        if event['type']=='message':
+                            print (event) 
+                            if event['text']=='hi':
+                                client.rtm_send_message(
+                                    event['channel'],
+                                    "Hello World!"
+                                )
                     except:
-                        print ("That doesn't work")
+                        print ("failed")
 
 
                 time.sleep(1)
