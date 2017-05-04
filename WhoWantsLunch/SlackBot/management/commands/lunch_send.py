240b13c5e6c26aaee4282fa6719db36677213988
Scott Culcheth <scott.culcheth@gmail.com>
2017-05-04 13:01:35 +0100

    Slackbot now posts a lunch request to all users (limited by if statement

--- WhoWantsLunch/SlackBot/management/commands/lunch_send.py
+++ WhoWantsLunch/SlackBot/management/commands/lunch_send.py
@@ -6,8 +6,16 @@ class Command(BaseCommand):
     help = 'Sends a message to all users about a new lunch plan'
     def handle(self, *args, **options):
         client = SlackClient(SlackbotConfig.team_key)
-        user_list = client.api_call( "users.list" )
-        channel_list = client.api_call( "channels.list" )
-        
-        print (user_list['members'][0])
-        print (channel_list['channels'][0])
+        user_list = client.api_call( "users.list" )['members']
+        channel_list = client.api_call( "channels.list" )['channels']
+        im_list = client.api_call( "im.list" )['ims']
+
+        for user in user_list:
+            if user['name'] == "scottc" or user['name'] == "simon" or "lalsoutar":
+                for im in im_list:
+                    if im['user'] == user['id']:
+                        client.api_call(
+                            "chat.postMessage",
+                            channel=im['id'],
+                            text="Would you like to join the team lunch at <THE PLACE> on <THE DATE> at <THE TIME>?"
+                        )
