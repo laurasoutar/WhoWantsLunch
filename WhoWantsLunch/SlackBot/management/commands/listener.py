770370b1d07832f40aa74e506efeea34afd4a8e5
Scott Culcheth <scott.culcheth@gmail.com>
2017-05-04 10:54:11 +0100

    Added initial slackbot listener. Run from python manage.py listener

---
+++ WhoWantsLunch/SlackBot/management/commands/listener.py
@@ -0,0 +1,18 @@
+### slackbot/management/commands/listener.py
+from slackclient import SlackClient
+from django.core.management.base import BaseCommand
+import time
+class Command(BaseCommand):
+    help = 'Starts the bot for the first'
+    def handle(self, *args, **options):
+        client = SlackClient('xoxb-179408173191-E8sjwhEkQf43ZUUdmMTyeUYD')
+        if client.rtm_connect():
+            while True:
+                events = client.rtm_read()
+                for event in events:
+                    if event['type']=='message' and event['text']=='hi':
+                        client.rtm_send_message(
+                            event['channel'],
+                            "Hello World!"
+                        )
+                time.sleep(1)
