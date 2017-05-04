718d9101a1937ff2512eaa717390d0a5af542554
Scott Culcheth <scott.culcheth@gmail.com>
2017-05-04 13:29:05 +0100

    Refactor to make client reusable within the instance

--- WhoWantsLunch/SlackBot/management/commands/listener.py
+++ WhoWantsLunch/SlackBot/management/commands/listener.py
@@ -4,21 +4,24 @@ from SlackBot.apps import SlackbotConfig
 import time
 class Command(BaseCommand):
     help = 'Listens for events on our slack teams client'
+
+    def __init__(self):
+        self.client = SlackClient(SlackbotConfig.team_key)
+
     def handle(self, *args, **options):
-        client = SlackClient(SlackbotConfig.team_key)
-        if client.rtm_connect():
+        if self.client.rtm_connect():
             while True:
-                events = client.rtm_read()
+                events = self.client.rtm_read()
                 for event in events:
                     try:
                         if event['type']=='message':
                             if event['text'].lower()=='yes':
-                                client.rtm_send_message(
+                                self.client.rtm_send_message(
                                     event['channel'],
                                     "Excellent, see you there!!!"
                                 )
                             if event['text'].lower()=='no':
-                                client.rtm_send_message(
+                                self.client.rtm_send_message(
                                     event['channel'],
                                     "Of all the humans, you are amongst the worst!"
                                 )
@@ -28,3 +31,9 @@ class Command(BaseCommand):
 
 
                 time.sleep(1)
+
+    def affirmative(self, channel):
+        pass
+
+    def denied(self, channel):
+        pass
