94b1ea6c4585ffdd433c7d3d0b6b9e7c5bbb1683
Scott Culcheth <scott.culcheth@gmail.com>
2017-05-04 17:21:57 +0100

    Completed the callback method for slack events

--- WhoWantsLunch/SlackBot/callback.py
+++ WhoWantsLunch/SlackBot/callback.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
+@csrf_exempt
 def process( request ):
-	return HttpResponse( "TESTING" )
+	post = request.POST
+	payload = post.get('payload')
+
+	j = json.loads(payload)
+	result = j['actions'][0]['value']
+	channel = j['channel']['id']
+
+	if result == "yes":
+		return HttpResponse("Excellent, see you there!!")
+
+	return HttpResponse("Of all the humans, you are amongst the worst!")
