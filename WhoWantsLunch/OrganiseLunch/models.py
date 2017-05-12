3fe40bdceb9acebf84c8f2040ee9cb8309afa408
Simon Wallace <simonwallace@users.noreply.github.com>
2017-05-12 19:44:08 +0100

    Corrected meal display name
    models.py:
    - Corrected the string display name of meals by correctly indenting the __str__ method into the Meal class.

--- WhoWantsLunch/OrganiseLunch/models.py
+++ WhoWantsLunch/OrganiseLunch/models.py
@@ -11,8 +11,8 @@ class Meal(models.Model):
     slack_channel = models.CharField(max_length=200, null=False, blank=False)
     notes = models.TextField()
 
-def __str__(self):
-    return self.meal_name
+    def __str__(self):
+        return self.meal_name
 
 class Order(models.Model):
     meal = models.ForeignKey(Meal)
