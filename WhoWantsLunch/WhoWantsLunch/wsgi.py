6ffd58249503fb637e91ab47197e68684b042405
Simon Wallace <simonwallace@users.noreply.github.com>
2017-05-29 20:40:17 +0100

    Corrected locale
    wsgi.py:
    - The locale is now set to the user's default setting when the application starts.

--- WhoWantsLunch/WhoWantsLunch/wsgi.py
+++ WhoWantsLunch/WhoWantsLunch/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
+import locale
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WhoWantsLunch.settings")
+
+locale.setlocale(locale.LC_ALL, "")
 
 application = get_wsgi_application()
